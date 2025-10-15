1a9d2ec49d88c7703ba6e6332388e2eeb7c1b0bd
Azer-khadhraoui <azerronaldo2004@gmail.com>
2025-10-15 17:33:29 +0100

    completer Prosit 5

--- Prosit1/src/zoo/entities/Animal.java
+++ Prosit1/src/zoo/entities/Animal.java
@@ -1,50 +1,55 @@
 package zoo.entities;
 
 public class Animal {
-    private String family;
-    private String name;
-    private int age;
-    private boolean isMammal;
+    protected String family;
+    protected String name;
+    protected int age;
+    protected boolean isMammal;
 
     public Animal() {}
 
     public Animal(String family, String name, int age, boolean isMammal) {
         this.family = family;
         this.name = name;
-        setAge(age); // vérification
+        this.age = age >= 0 ? age : 0;
         this.isMammal = isMammal;
     }
 
-    // Getters & Setters avec vérifications
-    public String getFamily() { return family; }
-    public void setFamily(String family) { this.family = family; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public int getAge() { return age; }
-    public void setAge(int age) {
-        if (age < 0) {
-            System.out.println("❌ L’âge d’un animal ne peut pas être négatif !");
-            this.age = 0;
-        } else {
-            this.age = age;
-        }
+    // ✅ Getters et Setters
+    public String getFamily() {
+        return family;
     }
 
-    public boolean isMammal() { return isMammal; }
-    public void setMammal(boolean mammal) { isMammal = mammal; }
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = Math.max(age, 0);
+    }
+
+    public boolean isMammal() {
+        return isMammal;
+    }
+
+    public void setMammal(boolean mammal) {
+        isMammal = mammal;
+    }
 
     @Override
     public String toString() {
-        return name + " (" + family + "), âge: " + age + ", mammifère: " + isMammal;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (!(obj instanceof Animal)) return false;
-        Animal other = (Animal) obj;
-        return this.name.equalsIgnoreCase(other.name);
+        return "Animal{name='" + name + "', family='" + family + "', age=" + age + ", mammifère=" + isMammal + "}";
     }
 }
--- Prosit1/src/zoo/management/ZooManagement.java
+++ Prosit1/src/zoo/management/ZooManagement.java
@@ -1,18 +1,33 @@
 package zoo.management;
 
-import zoo.entities.Animal;
-import zoo.entities.Zoo;
+import zoo.entities.*;
 
 public class ZooManagement {
     public static void main(String[] args) {
-        Zoo zoo1 = new Zoo("Belvédère", "Tunis");
-        Animal lion = new Animal("Felidae", "Simba", 5, true);
-        Animal tiger = new Animal("Felidae", "Shera", 3, true);
+        // Instanciation simple (Instruction 21)
+        Aquatic aquatic = new Aquatic();
+        Terrestrial terrestrial = new Terrestrial();
+        Dolphin dolphin = new Dolphin();
+        Penguin penguin = new Penguin();
 
-        zoo1.addAnimal(lion);
-        zoo1.addAnimal(tiger);
+        System.out.println("Création d'objets réussie sans erreur ✅");
 
-        zoo1.displayZoo();
-        zoo1.displayAnimals();
+        // Instanciation paramétrée (Instruction 22)
+        Aquatic aq1 = new Aquatic("Poisson", "Requin", 10, false, "Océan");
+        Terrestrial t1 = new Terrestrial("Félin", "Tigre", 5, true, 4);
+        Dolphin d1 = new Dolphin("Mammifère", "Flipper", 8, true, "Mer", 25.5f);
+        Penguin p1 = new Penguin("Oiseau", "Pingouin Empereur", 3, false, "Bassin", 10.2f);
+
+        System.out.println("\n=== toString() Test ===");
+        System.out.println(aq1);
+        System.out.println(t1);
+        System.out.println(d1);
+        System.out.println(p1);
+
+        // Test méthode swim() (Instruction 24)
+        System.out.println("\n=== Méthode swim() ===");
+        aq1.swim();
+        d1.swim();
+        p1.swim();
     }
 }
